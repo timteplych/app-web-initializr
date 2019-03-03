ca6cf7170b64c8b1256620d1aa41d7b5b508e043
timteplych <timteplych@mail.ru>
2019-03-03 21:12:11 +0800

    Initial commit

---
+++ src/main/java/ru/ttv/springboot/appwebinitializr/AppWebInitializrApplication.java
@@ -0,0 +1,13 @@
+package ru.ttv.springboot.appwebinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebInitializrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ttv/springboot/appwebinitializr/AppWebInitializrApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.ttv.springboot.appwebinitializr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppWebInitializrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
